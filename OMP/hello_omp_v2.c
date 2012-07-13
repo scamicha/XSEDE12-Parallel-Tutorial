d1fff35e5098934720189f416240d2561d8e31cd
Scott Michael <scamicha@iu.edu>
2012-07-12 21:30:11 -0400

    Submission scripts

--- OMP/hello_omp_v2.c
+++ OMP/hello_omp_v2.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 int main (int argc, char *argv[]) {
 
-int nthreads, tid;
+  int nthreads, tid;
 
-
+  omp_set_num_threads(6);
 #pragma omp master
- {
-   nthreads = omp_get_num_threads();
-   printf("There are %d threads available. \n", nthreads);
- }
+  {
+    nthreads = omp_get_num_threads();
+    printf("There are %d threads available. \n", nthreads);
+  }
 
  // Fork a team of threads and give a private copy of their thread number
 #pragma omp parallel private(tid)
- {
-   // Obtain thread number
+  {
+    // Obtain thread number
    tid = omp_get_thread_num();
    printf("Hello World from thread = %d\n", tid);
- }
+  }
 
 }
 
