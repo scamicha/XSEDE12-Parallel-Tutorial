9405f5b67f991f14c198169f6cb0bea612db658b
Scott Michael <scamicha@iu.edu>
2012-07-15 22:59:00 -0500

    Added hybrid

--- HYBRID/hello_hybrid_v1.c
+++ HYBRID/hello_hybrid_v1.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <omp.h>
 
 int main (argc, argv)
      int argc;
      char *argv[];
 {
   MPI_Init (&argc, &argv);	/* starts MPI */
-  printf( "Hello world!");
+  omp_set_num_threads(6);
+  // Fork a team of threads
+#pragma omp parallel
+  {
+    printf( "Hello world!");
+  }
   MPI_Finalize();              /* ends MPI */
   return 0;
 }
--- MPI/hello_mpi_v2.c
+++ MPI/hello_mpi_v2.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <mpi.h>
 
-
 int main (argc, argv)
      int argc;
      char *argv[];
